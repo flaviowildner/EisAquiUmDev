e0e9feda343dd10dfb5b0352fc73551ce27c385f
flaviowildner <flaviowildner@hotmail.com>
2026-06-04 13:20:59 -0300

    feat: automate database migrations on application startup

--- Application/entrypoint.sh
+++ Application/entrypoint.sh
@@ -6,9 +6,5 @@ until pg_isready -h postgres -U postgres; do
   sleep 1
 done
 
-echo "Postgres está pronto. Executando migrations..."
-cd /app
-dotnet ef database update
-
-echo "Migrations concluídas. Iniciando aplicação..."
+echo "Postgres está pronto. Iniciando aplicação..."
 exec dotnet Application.dll
